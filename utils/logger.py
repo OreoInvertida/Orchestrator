d10e5859f35dd6af31e536ab8e92a3888889f641
DamianDuque <damianduquel@gmail.com>
2025-05-05 16:32:29 -0500

    feat: logger

---
+++ utils/logger.py
@@ -0,0 +1,13 @@
+# orchestrator/utils/logger.py
+import logging
+
+logger = logging.getLogger("orchestrator_logger")
+logger.setLevel(logging.INFO)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s")
+console_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
